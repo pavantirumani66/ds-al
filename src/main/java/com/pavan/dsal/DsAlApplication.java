532d969cc5797ade8df1dca5fa4265b6c47c1e55
HYDERABAD LOCAL <HYDERABAD LOCAL@DESKTOP-G9NKDVG>
2021-01-01 20:29:19 +0530

    first

---
+++ src/main/java/com/pavan/dsal/DsAlApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.dsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAlApplication.class, args);
+	}
+
+}
